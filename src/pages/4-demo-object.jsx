961e5310a44ec1c8cd31c16703f769e38f1441d4
Alex Morelli <alexmorelli@CVG-C02RK01YG8WQ.local>
2020-07-24 09:12:07 -0400

    small change

--- src/pages/4-demo-object.jsx
+++ src/pages/4-demo-object.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {productCatalog} from "../demo-ts/constants";
+import {productCatalog} from "../demo/constants";
 
 const Index = () => {
   const productNames = Object.keys(productCatalog);
   const list = productNames.map(product => {
     return (
-    <div key={product}>
+    <div key={product} style={{display: 'flex', justifyContent: 'space-between', width: '400px'}}>
       <div>Product:{product}</div>
       <div>Price: {productCatalog[product].price}</div>
     </div>
